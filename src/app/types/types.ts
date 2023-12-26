46afd14e34df4a704903c495226f743d2cf43b5f
nacica <mikio.yokohama@gmail.com>
2023-12-26 17:01:31 +0900

    TypeScript追加

---
+++ src/app/types/types.ts
@@ -0,0 +1,11 @@
+type BookType = {
+    id: number;
+    title: string;
+    price: number;
+    content: string;
+    thumbnail: { url: string };
+    created_at: string;
+    updated_at: string;
+  };
+    
+  export type { BookType};
